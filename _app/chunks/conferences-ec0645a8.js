bc95cf715d671c08a04a84ef81ed20fe00307ab1
josanri <josanri@users.noreply.github.com>
2022-10-19 21:33:55 +0000

    deploy: 2b4b5450ff36d7b3186e322b1a5aa14ad6bb8d81

---
+++ _app/chunks/conferences-ec0645a8.js
@@ -0,0 +1,1 @@
+var e=[{title:"\u{1F6A7} Sab\xEDamos que eres un cotilla, pero no tanto \u{1F6A7}",description:"Secci\xF3n pendiente de actualizaci\xF3n, m\xE1s informaci\xF3n pr\xF3ximamente."}];export{e as c};
---
+++ _app/chunks/workshops-2b57d43b.js
@@ -0,0 +1,1 @@
+var e=[{title:"\u{1F528} Aprende una nueva tecnolog\xEDa emergente o ad\xE9ntrate en una existente \u{1F528}",description:"Pr\xF3ximamente en los laboratorios de la ETSII."}];export{e as w};
