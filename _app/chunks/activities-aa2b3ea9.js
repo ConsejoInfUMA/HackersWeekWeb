18c8efd1f1b04b62534525168c20dee1deba64a5
josanri <josanri@users.noreply.github.com>
2023-02-10 18:24:58 +0000

    deploy: 968cc391739b1fd5bf5563c7e46a2f38eb5bc3c3

---
+++ _app/chunks/activities-aa2b3ea9.js
@@ -0,0 +1,7 @@
+var a=[{title:"Sesiones de Rol",date:"23/24-02-22 - 16:30",description:`Vuelven las Sesiones de Rol a la Hackers Week. Traemos cuatro partidas los d\xEDas 23 y 24 de febrero desde las 16:30 a las 20:00 en la cuarta planta de biblioteca. Estas partidas se realizar\xE1n con tem\xE1tica relacionada con la tecnolog\xEDa  y son las siguientes:
+- Toma la pastilla y sonr\xEDe ;) - Cyberpunk RED (Jueves)
+- TheReaper: Tras la m\xE1quina - Nuestro \xDAltimo Verano (Jueves)
+- El Asedio de Arka - Anima: Beyond Fantasy (Viernes)
+- Terrores en la excavaci\xF3n - Starfinder (Viernes)
+Las partidas son aptas tanto para jugadores m\xE1s experimentados c\xF3mo cualquiera que quiera probar a jugar a rol por primera vez.
+\xA1Ap\xFAntate antes de que se acaben las plazas!`,ubicacion:"Biblioteca 4\xAA Planta",url:"http://u.uma.es/dn8"}];export{a};
